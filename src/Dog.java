8a2e80b07ddffd727478d8eab95cade135d61c0a
Nsun646 <nsun646@gmail.com>
2023-08-31 14:00:36 -0400

    project

---
+++ src/Dog.java
@@ -0,0 +1,11 @@
+class Dog {
+    private String furColor;
+    private String eyeColor;
+    private String name;
+
+    private int weight;
+
+    private String furLength;
+
+    public Dog(String furColor, String eyeColor, )
+}
